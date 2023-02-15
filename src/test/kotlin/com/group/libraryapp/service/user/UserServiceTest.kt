3af899f4bb2915dc25a197597a42bf3ba1653d21
Hyune-c <choi8608@gmail.com>
2023-02-16 00:28:54 +0900

    [도서관리 애플리케이션 리팩토링 준비하기] 8강. 유저 관련 기능 테스트 작성하기

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,8 +1,10 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
@@ -29,48 +31,48 @@ class UserServiceTest(
         assertThat(results[0].age).isNull()
     }
 
-//    test("유저 조회가 정상 동작한다") {
-//        // given
-//        userRepository.saveAll(
-//            listOf(
-//                User("A", 20),
-//                User("B", null),
-//            )
-//        )
-//
-//        // when
-//        val results = userService.getUsers()
-//
-//        // then
-//        assertThat(results).hasSize(2) // [UserResponse(), UserResponse()]
-//        assertThat(results).extracting("name").containsExactlyInAnyOrder("A", "B") // ["A", "B"]
-//        assertThat(results).extracting("age").containsExactlyInAnyOrder(20, null)
-//    }
-//
-//    test("유저 업데이트가 정상 동작한다") {
-//        // given
-//        val savedUser = userRepository.save(User("A", null))
-//        val request = UserUpdateRequest(savedUser.id!!, "B")
-//
-//        // when
-//        userService.updateUserName(request)
-//
-//        // then
-//        val result = userRepository.findAll()[0]
-//        assertThat(result.name).isEqualTo("B")
-//    }
-//
-//    test("유저 삭제가 정상 동작한다") {
-//        // given
-//        userRepository.save(User("A", null))
-//
-//        // when
-//        userService.deleteUser("A")
-//
-//        // then
-//        assertThat(userRepository.findAll()).isEmpty()
-//    }
-//
+    test("유저 조회가 정상 동작한다") {
+        // given
+        userRepository.saveAll(
+            listOf(
+                User("A", 20),
+                User("B", null),
+            )
+        )
+
+        // when
+        val results = userService.getUsers()
+
+        // then
+        assertThat(results).hasSize(2) // [UserResponse(), UserResponse()]
+        assertThat(results).extracting("name").containsExactlyInAnyOrder("A", "B") // ["A", "B"]
+        assertThat(results).extracting("age").containsExactlyInAnyOrder(20, null)
+    }
+
+    test("유저 업데이트가 정상 동작한다") {
+        // given
+        val savedUser = userRepository.save(User("A", null))
+        val request = UserUpdateRequest(savedUser.id!!, "B")
+
+        // when
+        userService.updateUserName(request)
+
+        // then
+        val result = userRepository.findAll()[0]
+        assertThat(result.name).isEqualTo("B")
+    }
+
+    test("유저 삭제가 정상 동작한다") {
+        // given
+        userRepository.save(User("A", null))
+
+        // when
+        userService.deleteUser("A")
+
+        // then
+        assertThat(userRepository.findAll()).isEmpty()
+    }
+
 //    test("대출 기록이 없는 유저도 응답에 포함된다") {
 //        // given
 //        userRepository.save(User("A", null))
@@ -83,7 +85,7 @@ class UserServiceTest(
 //        assertThat(results[0].name).isEqualTo("A")
 //        assertThat(results[0].books).isEmpty()
 //    }
-//
+
 //    test("대출 기록이 많은 유저의 응답이 정상 동작한다") {
 //        // given
 //        val savedUser = userRepository.save(User("A", null))
