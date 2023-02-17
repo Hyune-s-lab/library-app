1aa68c8f029b08578dc5c882004cb0968271cafe
Hyune-c <choi8608@gmail.com>
2023-02-18 00:29:26 +0900

    [두 번째 요구사항 추가하기 - 도서 대출 현황] 26강. 유저 대출 현황 보여주기 - 테스트 코드 개발

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -2,7 +2,9 @@ package com.group.libraryapp.service.user
 
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import io.kotest.core.spec.style.FunSpec
@@ -73,42 +75,40 @@ class UserServiceTest(
         assertThat(userRepository.findAll()).isEmpty()
     }
 
-//    test("대출 기록이 없는 유저도 응답에 포함된다") {
-//        // given
-//        userRepository.save(User("A", null))
-//
-//        // when
-//        val results = userService.getUserLoanHistories()
-//
-//        // then
-//        assertThat(results).hasSize(1)
-//        assertThat(results[0].name).isEqualTo("A")
-//        assertThat(results[0].books).isEmpty()
-//    }
+    test("대출 기록이 없는 유저도 응답에 포함된다") {
+        // given
+        userRepository.save(User("A", null))
 
-//    test("대출 기록이 많은 유저의 응답이 정상 동작한다") {
-//        // given
-//        val savedUser = userRepository.save(User("A", null))
-//        userLoanHistoryRepository.saveAll(
-//            listOf(
-//                UserLoanHistory.fixture(savedUser, "책1", UserLoanStatus.LOANED),
-//                UserLoanHistory.fixture(savedUser, "책2", UserLoanStatus.LOANED),
-//                UserLoanHistory.fixture(savedUser, "책3", UserLoanStatus.RETURNED),
-//            )
-//        )
-//
-//        // when
-//        val results = userService.getUserLoanHistories()
-//
-//        // then
-//        assertThat(results).hasSize(1)
-//        assertThat(results[0].name).isEqualTo("A")
-//        assertThat(results[0].books).hasSize(3)
-//        assertThat(results[0].books).extracting("name")
-//            .containsExactlyInAnyOrder("책1", "책2", "책3")
-//        assertThat(results[0].books).extracting("isReturn")
-//            .containsExactlyInAnyOrder(false, false, true)
-//    }
+        // when
+        val results = userService.getUserLoanHistories()
+
+        // then
+        assertThat(results).hasSize(1)
+        assertThat(results[0].name).isEqualTo("A")
+        assertThat(results[0].books).isEmpty()
+    }
+
+    test("대출 기록이 많은 유저의 응답이 정상 동작한다") {
+        // given
+        val savedUser = userRepository.save(User("A", null))
+        userLoanHistoryRepository.saveAll(
+            listOf(
+                UserLoanHistory(savedUser, "책1", UserLoanStatus.LOANED),
+                UserLoanHistory(savedUser, "책2", UserLoanStatus.LOANED),
+                UserLoanHistory(savedUser, "책3", UserLoanStatus.RETURNED),
+            )
+        )
+
+        // when
+        val results = userService.getUserLoanHistories()
+
+        // then
+        assertThat(results).hasSize(1)
+        assertThat(results[0].name).isEqualTo("A")
+        assertThat(results[0].books).hasSize(3)
+        assertThat(results[0].books).extracting("name").containsExactlyInAnyOrder("책1", "책2", "책3")
+        assertThat(results[0].books).extracting("isReturn").containsExactlyInAnyOrder(false, false, true)
+    }
 }) {
     override suspend fun afterEach(testCase: TestCase, result: TestResult) {
         super.afterEach(testCase, result)
